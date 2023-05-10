a35ad9bf93f9b28a78b0904a407f1232883f4d2d
lvliqi <lvliqi_d@163.com>
2023-05-10 23:34:28 +0800

    feat: add test

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -3,17 +3,19 @@ import asyncDebounce from '@/index';
 
 test('base', async () => {
   let count = 1;
-  const d = asyncDebounce(() => new Promise(resolve => {
+  let _args:number[] = [];
+  const d = asyncDebounce((...args: number[]) => new Promise(resolve => {
     setTimeout(() => {
+      _args = args;
       count++;
       resolve(1);
     }, 300);
   }), 200);
 
   const results = await Promise.all([
-    d(),
-    d(),
-    d()
+    d(1, 2),
+    d(2, 3),
+    d(3, 4)
   ]);
 
   expect(count).toBe(2);
@@ -22,6 +24,7 @@ test('base', async () => {
     1,
     1
   ]);
+  expect(_args).toEqual([3, 4]);
 
 
 });
